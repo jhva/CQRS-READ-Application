4ba5bcfea88e3f6cfd587a404887f8db3353e3e0
jhva <junghun8158@naver.com>
2023-05-11 19:56:59 +0900

    :tada: build: init commit

---
+++ src/test/java/com/example/readelastic/ReadElasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.readelastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadElasticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
